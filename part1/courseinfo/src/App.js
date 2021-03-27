d105b1430a44e3438c11d13434c30a29caa41bcf
turunenv <vili.turunen@aalto.fi>
2021-03-27 10:09:46 +0200

    Making code more readable in courseinfo :)

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -11,12 +11,20 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-      <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-      <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+      <Part 
+        part={props.course.parts[0].name} 
+        exercises={props.course.parts[0].exercises} 
+      />
+      <Part 
+        part={props.course.parts[1].name} 
+        exercises={props.course.parts[1].exercises} 
+      />
+      <Part 
+        part={props.course.parts[2].name} 
+        exercises={props.course.parts[2].exercises} 
+      />
     </div>
-  )
- 
+  ) 
 }
 
 const Total = (props) => {
