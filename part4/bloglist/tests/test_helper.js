fd24acd0a11dc38f7a75eebc8c75e9042f60f04b
turunenv <vili.turunen@aalto.fi>
2022-06-22 18:38:09 +0300

    helper files for testing the blog api

---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,26 @@
+const Blog = require('../models/blog');
+
+const initialBlogs = [
+    {
+        title: "JS is awesome",
+        author: "Charles Babbage",
+        url: "here.com",
+        likes: 10
+    },
+    {
+        title: "Python is also awesome",
+        author: "Ada Lovelace",
+        url: "there.fi",
+        likes: 1000,
+    }
+];
+
+const blogsInDb = async () => {
+    const blogs = await Blog.find({});
+    return blogs.map(blog => blog.toJSON());
+}
+
+module.exports = {
+    initialBlogs,
+    blogsInDb,
+}
