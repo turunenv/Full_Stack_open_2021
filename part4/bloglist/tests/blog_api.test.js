199ab05e098b60d0abea918800b38b7c0b901858
turunenv <vili.turunen@aalto.fi>
2022-06-22 18:36:40 +0300

    tests for the blog api

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+
+const Blog = require('../models/blog');
+
+const api = supertest(app);
+const helper = require('./test_helper');
+
+beforeEach(async() => {
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+});
+
+test('all blogs are returned in json format', async () => {
+    const response = await api
+                            .get('/api/blogs')
+                            .expect(200)
+                            .expect('Content-Type', /application\/json/)
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+    
+})
