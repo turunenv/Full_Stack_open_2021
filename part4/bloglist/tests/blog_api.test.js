4d5f025da81ec04494f2965dabe650cc04847a43
turunenv <vili.turunen@aalto.fi>
2022-06-23 16:45:49 +0300

    4.10 POST to /api/blogs passes tests after refactoring middleware to use async/await

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -12,7 +12,7 @@ beforeEach(async() => {
     await Blog.insertMany(helper.initialBlogs);
 });
 
-test('all blogs are returned in json format', async () => {
+test('all blogs are returned in json format', async() => {
     const response = await api
                             .get('/api/blogs')
                             .expect(200)
@@ -21,9 +21,30 @@ test('all blogs are returned in json format', async () => {
     
 });
 
-test('all blogs have the id property', async () => {
+test('all blogs have the id property', async() => {
     const response = await api.get('/api/blogs');
     
     //check that all blogs have the id property
     response.body.forEach(blog => expect(blog.id).toBeDefined());
+});
+
+test('POST to /api/blogs successfully saves the new blog to the database', async() => {
+    const newBlog = {
+        title: "grinding HTML",
+        author: "Veku Turunen",
+        url: "mokkilife.fi",
+        likes: 999,
+    };
+
+    await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+    const blogsAfterPosting = await helper.blogsInDb();
+    expect(blogsAfterPosting).toHaveLength(helper.initialBlogs.length + 1);
+
+    const blogTitles = blogsAfterPosting.map(blog => blog.title);
+    expect(blogTitles).toContain("grinding HTML");
 })
