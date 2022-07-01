ffa3622d5d67c7958add6cd16976e057c3bfb70d
turunenv <vili.turunen@aalto.fi>
2022-07-01 19:35:45 +0300

    app using loginRouter

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require('cors');
 const blogRouter = require('./controllers/blogs');
 const userRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -24,5 +25,6 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', userRouter);
+app.use('/api/login', loginRouter);
 
 module.exports = app;
