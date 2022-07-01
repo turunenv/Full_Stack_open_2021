88f0e9b2c5819449a44136ea35ff21767f2057c1
turunenv <vili.turunen@aalto.fi>
2022-07-01 16:58:27 +0300

    4.16 - username and password validation

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,6 +5,19 @@ const User = require('../models/user');
 userRouter.post('/', async (req, res) => {
     const { username, name, password } = req.body;
 
+    if (username.length < 3 || password.length < 3) {
+        return res.status(400).json({
+                error: 'username and password must be at least 3 characters long'
+            });
+    }
+
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+        return res.status(400).json({
+            error: 'username must be unique'
+        })
+    }
+
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
 
