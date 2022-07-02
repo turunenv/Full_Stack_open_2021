e6b2da22f6184dd8f7f30c2030aa0ab94df05cfe
turunenv <vili.turunen@aalto.fi>
2022-07-02 15:08:08 +0300

    added errorHandler middleware

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -6,8 +6,30 @@ const requestLogger = (req, res, next) => {
     logger.info('Body:', req.body);
     logger.info('----');
     next();
+};
+
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message);
+    console.log("ARE WE IN THE ERROR HANDLER?? -- error.name:", error.name)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({
+          error: 'malformatted id'
+        })
+      } else if (error.name === 'ValidationError') {
+        return response.status(400).json({
+          error: error.message 
+        })
+      } else if (error.name === 'JsonWebTokenError') {
+        return response.status(401).json({
+          error: 'invalid token'
+        })
+    }
+
+    next(error);
 }
 
 module.exports = {
     requestLogger,
+    errorHandler
 }
