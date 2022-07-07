613879293a8bdc66e9cd1832d2bb916ca2f3fa49
turunenv <vili.turunen@aalto.fi>
2022-07-07 15:50:32 +0300

    components for login functionality

---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,26 @@
+const LoginForm = (props) => (
+    <form onSubmit={props.handleLogin}>
+        <div>
+                <label htmlFor="Username">username</label>
+                <input 
+                type="text" 
+                value={props.username}
+                name="Username"
+                id="Username"
+                onChange={(event) => props.setUsername(event.target.value)}
+                />
+        </div>
+        <div>
+                <label htmlFor="Password">password</label>
+                <input type="text"
+                value={props.password}
+                name="Password"
+                id="Password"
+                onChange={(event) => props.setPassword(event.target.value)}
+                />
+        </div>
+        <button type="submit">login</button>
+    </form>
+)
+
+export default LoginForm
