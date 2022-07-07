684e18d235cce9336ec24ca8642e88676da588d8
turunenv <vili.turunen@aalto.fi>
2022-07-07 15:51:17 +0300

    5.1 - added basic login functionality

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,44 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+      console.log(user)
+      setUser(user)
+      setPassword('')
+      setUsername('')
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  //render login-form if user is not logged in
+  if (user === null) {
+    return (
+      <>
+        <h2>Login to the application</h2>
+        <LoginForm 
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      </>
+      
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <div className='loggedInUser'>{user.name} logged in</div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
