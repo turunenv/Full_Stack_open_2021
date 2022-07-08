a830a13fdc5437517c9e63a125a2527ce422d03b
turunenv <vili.turunen@aalto.fi>
2022-07-08 16:45:28 +0300

    form component for adding new blogs

---
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const BlogForm = ({ createBlog }) => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+
+    const createNewBlog = () => {
+        const newBlog = {
+            title,
+            author,
+            url,
+        }
+
+        createBlog(newBlog)
+    }
+
+    return (
+        <form onSubmit={createNewBlog} className="blogForm">
+            <label htmlFor="title">title:</label>
+            <input type="text" name="title" id="title" 
+                   value={title}
+                   onChange={({target}) => setTitle(target.value)}
+            />
+
+            <label htmlFor="author">author:</label>
+            <input type="text" name="author" id="author" 
+                   value={author}
+                   onChange={({target}) => setAuthor(target.value)}
+            />
+
+            <label htmlFor="url">url:</label>
+            <input type="text" name="url" id="url" 
+                   value={url} 
+                   onChange={({target}) => setUrl(target.value)}
+            />
+
+            <button type="submit">create</button>
+        </form>
+    )
+}
+
+export default BlogForm
