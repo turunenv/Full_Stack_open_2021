a3f9d5e55f107823fec4cdc7317f52b18cd12c4d
turunenv <vili.turunen@aalto.fi>
2022-07-10 15:44:58 +0300

    5.9 - blogs are sorted by the number of likes

--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -4,6 +4,9 @@ const BlogList = ({ blogs, updateBlog }) => {
     const blogListStyle = {
         marginTop: 10,
     }
+    // sort the blogs by the number of likes in descending order
+    blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
+
     return (
         <div style={blogListStyle}>
             {blogs.map(blog => {
