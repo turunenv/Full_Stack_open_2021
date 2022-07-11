54afa054e5ee363fed6e7905223aa00cd7fd8fab
turunenv <vili.turunen@aalto.fi>
2022-07-11 18:04:54 +0300

    5.13 - test for Blog, only title and author shown by default

---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+
+test('renders title and author, but not url and likes by default', () => {
+  const blog = {
+    author: 'Verneri Turunen',
+    id: '123123123123',
+    likes: 15,
+    title: 'The Wonders of CSS',
+    url: 'css-land.com',
+    user: {
+      id: '456456456',
+      name: 'Verneri Turunen',
+      username: 'Veku'
+    }
+  }
+
+  render(<Blog blog={blog} />)
+
+  //check that title and author are rendered on screen
+  const titleElem = screen.getByText('The Wonders of CSS')
+  expect(titleElem).toBeDefined()
+
+  const authorElem = screen.getByText('Verneri Turunen')
+  expect(authorElem).toBeDefined()
+
+  //check that url and likes are not rendered by default
+  const urlElem = screen.queryByText('css-land.com')
+  expect(urlElem).toBe(null)
+
+  const likesElem = screen.queryByText('likes', { exact:false })
+  expect(likesElem).toBe(null)
+})
