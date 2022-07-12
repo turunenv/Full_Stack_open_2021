2b6dc2dbb2fe593434365d7f88d8d563d24bca0d
turunenv <vili.turunen@aalto.fi>
2022-07-12 08:49:21 +0300

    5.15 - event handler clicked twice when like-button is clicked twice

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -47,3 +47,24 @@ test('renders url and likes after user clicks the view-button', async() => {
   screen.getByText('likes 15')
   screen.getByText('css-land.com')
 })
+
+test('event handler from props called twice when like button is clicked twice', async() => {
+  const user = userEvent.setup()
+
+  //onClick callback in the like-button is increaseLikesByOne, which calls updateBlog-function
+  //received as props -> we will mock this function
+  const updateBlog = jest.fn()
+  render(<Blog blog={blog} updateBlog={updateBlog} />)
+
+  //user clicks the view button
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  //user clicks the like button twice
+  const likeButton = screen.getByText('like')
+  await user.dblClick(likeButton)
+
+  //expect event handler to have been called twice
+  expect(updateBlog.mock.calls).toHaveLength(2)
+
+})
