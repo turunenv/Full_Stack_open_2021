8f620b4f40287a6006e189abe4aea2cb9caa2191
turunenv <vili.turunen@aalto.fi>
2022-07-13 13:54:02 +0300

    Notification not displayed when state is an empty string

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = 'Test Message'
+const initialState = ''
 
 const notificationSlice = createSlice({
     name: 'notification',
@@ -8,8 +8,12 @@ const notificationSlice = createSlice({
     reducers: {
         addNotification: (state, action) => {
             return action.payload
+        },
+        removeNotification: (state, action) => {
+            return ''
         }
     }
 })
 
+export const { addNotification, removeNotification } = notificationSlice.actions
 export default notificationSlice.reducer
