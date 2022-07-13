d9e71c35f8f28d092e606c971cd7fa6fb0fdc2ca
turunenv <vili.turunen@aalto.fi>
2022-07-13 16:15:22 +0300

    6.13

---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+const getAll = async () => {
+    const response = await axios.get(baseUrl)
+    return response.data
+}
+
+const services = {
+    getAll,
+}
+
+export default services
+
