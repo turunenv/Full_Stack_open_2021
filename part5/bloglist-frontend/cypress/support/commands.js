9ba28809ad51c1c431ec391170dcf472e68a1748
turunenv <vili.turunen@aalto.fi>
2022-07-14 13:55:31 +0300

    added cypress command for logging in

--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', ({ username, password }) => {
+  cy.request('POST', 'http://localhost:3003/api/login', {
+    username,
+    password,
+  }).then(response => {
+    const user = response.body
+    localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+    cy.visit('http://localhost:3000')
+  })
+})
