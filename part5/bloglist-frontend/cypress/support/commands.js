c3d4c0e2ca8872ba779e162911270db8f3d5b13f
turunenv <vili.turunen@aalto.fi>
2022-07-14 15:14:21 +0300

    added command for creating an initial blog

--- part5/bloglist-frontend/cypress/support/commands.js
+++ part5/bloglist-frontend/cypress/support/commands.js
@@ -34,3 +34,19 @@ Cypress.Commands.add('login', ({ username, password }) => {
     cy.visit('http://localhost:3000')
   })
 })
+
+Cypress.Commands.add('addBlog', ({ title, author, url}) => {
+  cy.request({
+    url: 'http://localhost:3003/api/blogs',
+    method: 'POST',
+    body: { title, author, url },
+    //to set the Authorization header needed in the POST-request,
+    //get the loggedBlogappUser JSON-string from localStorage, parse it into a JS-object, and access the 
+    //'token'-property 
+    headers: {
+      'Authorization': `bearer ${JSON.parse(localStorage.getItem('loggedBlogappUser')).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
+})
