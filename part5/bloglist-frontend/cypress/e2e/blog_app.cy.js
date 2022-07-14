1099cc8b41e443a33e9c20951904751c2f33760b
turunenv <vili.turunen@aalto.fi>
2022-07-14 13:29:12 +0300

    5.18 - tests for logging in with correct and incorrect credentials

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,16 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    
+    //add a user to the database
+    const user = {
+        name: 'Usain Bolt',
+        username: 'UZain',
+        password: 'fastman'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users', user)
+
+    //go to the front page of the app
     cy.visit('http://localhost:3000')
   })
 
@@ -16,5 +26,26 @@ describe('Blog app', function() {
 
     //check that the login-button is found
     cy.get('.login-button').contains('login')
+  })
+
+  describe('Login', function() {
+    it('succeeds with correct credentials', function() {
+      //get input elements, type credentials and click the login-button
+      cy.get('#Username').type('UZain')
+      cy.get('#Password').type('fastman')
+      cy.get('.login-button').click()
+
+      cy.contains('Usain Bolt logged in')      
+    })
+
+    it('fails with wrong credentials', function() {
+      //get input elements, type credentials and click the login-button
+      cy.get('#Username').type('UZain')
+      cy.get('#Password').type('wrong')
+      cy.get('.login-button').click()
+
+      //error-message rendered to the page?
+      cy.contains('wrong username or password')
+    })
   })
 })
