3ca479f2858a36de59095defcb5c8ba3fd2ba893
turunenv <vili.turunen@aalto.fi>
2022-07-15 10:15:56 +0300

    6.19 - Notification component uses connect

--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,18 +1,26 @@
-import { useSelector } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Notification = () => {
-  const notification = useSelector(state => state.notification)
+const Notification = (props) => {
+  console.log(props)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    display: notification === '' ? 'none' : '',
+    display: props.notification === '' ? 'none' : '',
   }
   return (
     <div style={style}>
-      {notification}
+      {props.notification}
     </div>
   )
 }
 
-export default Notification
+const mapStateToProps = state => {
+  return {
+    notification: state.notification,
+  }
+}
+
+const ConnectedNotification = connect(mapStateToProps)(Notification)
+
+export default ConnectedNotification
