9828a5497d7da9b9bb195ad695afaf360f596ac8
turunenv <vili.turunen@aalto.fi>
2022-07-22 12:35:05 +0300

    switched to double quotes and semicolons

--- part5/bloglist-frontend/.eslintrc.js
+++ part5/bloglist-frontend/.eslintrc.js
@@ -17,8 +17,6 @@ module.exports = {
   rules: {
     indent: ["error", 2],
     "linebreak-style": ["error", "windows"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": ["error", "always"],
