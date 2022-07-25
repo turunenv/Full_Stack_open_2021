3beaef10019839d83851c60c492e8b5491261cb3
turunenv <vili.turunen@aalto.fi>
2022-07-25 07:45:16 +0300

    refactoring

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,20 +1,57 @@
-import Home from "./components/Home";
+import Blogs from "./components/Blogs";
+import Blog from "./components/Blog";
 import Users from "./components/Users";
+import User from "./components/User";
+import Navbar from "./components/Navbar";
+
+import { fetchBlogs } from "./reducers/blogSlice";
+import { storeUser } from "./reducers/userSlice";
+import blogService from "./services/blogs";
+
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 //react router imports
 import {
   BrowserRouter as Router,
-  Routes, Route, Link
+  Routes, Route, Navigate //,Link
 } from "react-router-dom";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  //this needs to be in the app, not the Blogs.js so we fetch the blogs when refreshing on the other views
+  useEffect(() => {
+    dispatch(fetchBlogs);
+  }, []);
+
+  //login to persist when refreshing the page by checking if user is set in the local storage
+  useEffect(() => {
+    console.log(
+      "useEffect fired: checking if user-JSON has been set in the window.localStorage"
+    );
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      dispatch(storeUser(user));
+      blogService.setToken(user.token);
+    }
+  }, []);
   return (
-    <Router>
-      <Routes>
-        <Route path="/users" element={<Users />}/>
-      </Routes>
-    </Router>
-  )
+    <div>
+      <Router>
+        <Navbar />
+        <h2>Blog-app</h2>
+        <Routes>
+          <Route path="/" element={<Navigate to="/blogs"/>} />
+          <Route path="/blogs" element={<Blogs />}/>
+          <Route path="/blogs/:id" element={<Blog />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/users/:id" element={<User />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- part5/bloglist-frontend/src/components/BlogList.js
+++ part5/bloglist-frontend/src/components/BlogList.js
@@ -1,14 +1,21 @@
-import PropTypes from "prop-types";
+//import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
-
-import Blog from "./Blog";
+import { Link } from "react-router-dom";
 
 const BlogList = () => {
-  const blogs = useSelector(state => state.blogs);
+  const blogs = useSelector((state) => state.blogs);
+
   const blogListStyle = {
     marginTop: 10,
     padding: 0,
   };
+
+  const blogLinkStyle = {
+    marginTop: 5,
+    border: "2px solid black",
+    paddingTop: 4,
+    paddingLeft: 3,
+  }
   // sort the blogs by the number of likes in descending order
   const sortedBlogs = [...blogs].sort(
     (blog1, blog2) => blog2.likes - blog1.likes
@@ -18,20 +25,23 @@ const BlogList = () => {
     <ul style={blogListStyle}>
       {sortedBlogs.map((blog) => {
         return (
-          <Blog
-            key={blog.id}
-            blog={blog}
-          />
+          <li key={blog.id} style={blogLinkStyle}>
+            <Link to={`/blogs/${blog.id}`}>
+              <em>{blog.title}</em> by {blog.author}
+            </Link>
+          </li>
         );
       })}
     </ul>
   );
 };
 
-BlogList.propTypes = {
-  blogs: PropTypes.array.isRequired,
-  updateBlog: PropTypes.func.isRequired,
-  deleteBlog: PropTypes.func.isRequired,
-};
+//proptypes not in use anymore after implementing redux into the project
+
+// BlogList.propTypes = {
+//   blogs: PropTypes.array.isRequired,
+//   updateBlog: PropTypes.func.isRequired,
+//   deleteBlog: PropTypes.func.isRequired,
+// };
 
 export default BlogList;
